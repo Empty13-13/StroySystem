4a75f72df88565bc8f80fb019d9b4b6dc0630b24
Empty13-13 <maks.ept1@gmail.com>
2021-07-24 23:21:33 +0500

    Finish project V1.0

---
+++ #src/js/app.js
@@ -0,0 +1,6 @@
+@@include('files/dynamic_adapt.js', {})
+@@include('files/sliders.js', {})
+@@include('files/functions.js', {})
+@@include('files/forms.js', {})
+@@include('files/scroll.js', {})
+@@include('files/script.js', {})
---
+++ #src/js/files/sliders.js
@@ -0,0 +1,195 @@
+//BildSlider
+let sliders = document.querySelectorAll('._swiper');
+if (sliders) {
+	for (let index = 0; index < sliders.length; index++) {
+		let slider = sliders[index];
+		if (!slider.classList.contains('swiper-bild')) {
+			let slider_items = slider.children;
+			if (slider_items) {
+				for (let index = 0; index < slider_items.length; index++) {
+					let el = slider_items[index];
+					el.classList.add('swiper-slide');
+				}
+			}
+			let slider_content = slider.innerHTML;
+			let slider_wrapper = document.createElement('div');
+			slider_wrapper.classList.add('swiper-wrapper');
+			slider_wrapper.innerHTML = slider_content;
+			slider.innerHTML = '';
+			slider.appendChild(slider_wrapper);
+			slider.classList.add('swiper-bild');
+
+			if (slider.classList.contains('_swiper_scroll')) {
+				let sliderScroll = document.createElement('div');
+				sliderScroll.classList.add('swiper-scrollbar');
+				slider.appendChild(sliderScroll);
+			}
+		}
+		if (slider.classList.contains('_gallery')) {
+			//slider.data('lightGallery').destroy(true);
+		}
+	}
+	sliders_bild_callback();
+}
+
+function sliders_bild_callback(params) { }
+
+let sliderScrollItems = document.querySelectorAll('._swiper_scroll');
+if (sliderScrollItems.length > 0) {
+	for (let index = 0; index < sliderScrollItems.length; index++) {
+		const sliderScrollItem = sliderScrollItems[index];
+		const sliderScrollBar = sliderScrollItem.querySelector('.swiper-scrollbar');
+		const sliderScroll = new Swiper(sliderScrollItem, {
+			observer: true,
+			observeParents: true,
+			direction: 'vertical',
+			slidesPerView: 'auto',
+			freeMode: true,
+			scrollbar: {
+				el: sliderScrollBar,
+				draggable: true,
+				snapOnRelease: false
+			},
+			mousewheel: {
+				releaseOnEdges: true,
+			},
+		});
+		sliderScroll.scrollbar.updateSize();
+	}
+}
+
+
+function sliders_bild_callback(params) { }
+
+if(document.querySelector('.photos__swiper')){
+	new Swiper('.photos__swiper', {
+
+    // effect: 'fade',
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+
+		observer: true,
+		observeParents: true,
+		slidesPerView: 3,
+		slidesPerGroup: 3,
+		spaceBetween: 96,
+		autoHeight: true,
+		speed: 800,
+		//touchRatio: 0,
+		//simulateTouch: false,
+		loop: true,
+		//preloadImages: false,
+		//lazy: true,
+		// Dotts
+		pagination: {
+			el: '.swiper-photos__dotts',
+			clickable: true,
+		},
+		// Arrows
+		navigation: {
+			nextEl: '.photos-arrow_next',
+			prevEl: '.photos-arrow_prev',
+		},
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+				slidesPerGroup: 1,
+        spaceBetween: 0,
+        autoHeight: true,
+      },
+      768: {
+        slidesPerView: 2,
+				slidesPerGroup: 2,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 3,
+				slidesPerGroup: 3,
+        spaceBetween: 50,
+      },
+      1268: {
+        slidesPerView: 3,
+				slidesPerGroup: 3,
+        spaceBetween:90,
+      },
+    },
+
+		on: {
+			lazyImageReady: function () {
+				ibg();
+			},
+		}
+		// And if we need scrollbar
+		//scrollbar: {
+		//	el: '.swiper-scrollbar',
+		//},
+	});
+}
+
+if(document.querySelector('.feedback__swiper')){
+	new Swiper('.feedback__swiper', {
+
+		// effect: 'fade',
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false,
+		},
+
+		observer: true,
+		observeParents: true,
+		slidesPerView: 1,
+		spaceBetween: 0,
+		autoHeight: true,
+		speed: 800,
+		//touchRatio: 0,
+		//simulateTouch: false,
+		loop: true,
+		//preloadImages: false,
+		//lazy: true,
+		// Dotts
+		pagination: {
+			el: '.swiper-feedback__dotts',
+			clickable: true,
+		},
+		// Arrows
+		navigation: {
+			nextEl: '.swiper-feedback_next',
+			prevEl: '.swiper-feedback_prev',
+		},
+		// breakpoints: {
+		// 	320: {
+		// 		slidesPerView: 1,
+		// 		slidesPerGroup: 1,
+		// 		spaceBetween: 0,
+		// 		autoHeight: true,
+		// 	},
+		// 	768: {
+		// 		slidesPerView: 2,
+		// 		slidesPerGroup: 2,
+		// 		spaceBetween: 20,
+		// 	},
+		// 	992: {
+		// 		slidesPerView: 3,
+		// 		slidesPerGroup: 3,
+		// 		spaceBetween: 50,
+		// 	},
+		// 	1268: {
+		// 		slidesPerView: 3,
+		// 		slidesPerGroup: 3,
+		// 		spaceBetween:90,
+		// 	},
+		// },
+
+		on: {
+			lazyImageReady: function () {
+				ibg();
+			},
+		}
+		// And if we need scrollbar
+		//scrollbar: {
+		//	el: '.swiper-scrollbar',
+		//},
+	});
+}
